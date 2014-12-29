61c9ad9c698dfa1e04a246061eb2258c0809695d
ivailozd <ivailozd@gmail.com>
2014-12-29 22:48:08 +0200

    some kind of login

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -1,8 +1,9 @@
-app.controller('LoginController', function($scope, $location, authRequester){
+app.controller('LoginController', function($scope, $rootScope, $location, authRequester){
     $scope.login = function(){
         authRequester.loginUser($scope.user.username, $scope.user.password)
             .then(function(data){
-                $scope.accessToken = data.access_token;
+                $rootScope.accessToken = data.access_token;
+                $location.path('#/');
             }, function(error){
                 $scope.loginError = error.error_description;
             })
